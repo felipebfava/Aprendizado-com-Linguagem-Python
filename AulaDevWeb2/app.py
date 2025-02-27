810c8c034540a99876aa5ea60d8d8b5647b242d5
felipebfava <129697196+felipebfava@users.noreply.github.com>
2025-02-27 11:56:29 -0300

    Add files via upload

---
+++ AulaDevWeb2/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect 
+import socket, os 
+import sqlite3
+
+app = Flask(__name__) 
+
+def connect_db(): 
+    return sqlite3.connect("database.db")
+
+@app.route('/oi')
+def hello(): 
+    hostname = socket.gethostname() 
+    port = os.environ.get('PORT', '5000')  # Pega a porta do ambiente ou usa 5000 como padrão 
+    return f"Hello from {hostname} on port {port}!\n"
+ 
+@app.route('/')
+def index(): 
+    con = connect_db() # conecta ao banco
+    cur = con.cursor() # do sqlite
+    cur.execute("SELECT * FROM tasks") 
+    tasks = cur.fetchall() # converte resultado em lista
+    con.close() 
+    return render_template("index.html", tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task(): 
+    task = request.form['task'] 
+    con = connect_db() 
+    cur = con.cursor() 
+    cur.execute("INSERT INTO tasks (task, completed) VALUES (?, ?)", (task, 0)) 
+    con.commit() 
+    con.close()
+    return redirect('/') 
+
+@app.route('/delete/<int:id>')
+def delete_task(id): 
+    con = connect_db() 
+    cur = con.cursor() 
+    cur.execute("DELETE FROM tasks WHERE id=?", (id,)) 
+    con.commit() 
+    con.close() 
+    return redirect('/') 
+
+@app.route('/complete/<int:id>')
+def complete_task(id): 
+    con = connect_db() 
+    cur = con.cursor() 
+    cur.execute("UPDATE tasks SET completed = 1 WHERE id=?", (id,)) 
+    con.commit()
+    con.close() 
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True) 
